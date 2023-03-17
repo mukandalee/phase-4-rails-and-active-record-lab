064be04a60974d592d0daca9f02d76b60567c3d4
mukandalee <leonardmukanda254@gmail.com>
2023-03-17 09:15:32 +0300

    completed the lab

---
+++ app/models/student.rb
@@ -0,0 +1,15 @@
+class Student < ApplicationRecord
+    attr_accessor :first_name, :last_name
+
+    def initialize(first_name, last_name)
+      @first_name = first_name
+      @last_name = last_name
+    end
+  
+    def to_s
+      "#{first_name} #{last_name}"
+    end
+    # def to_s
+    #     "#{self.first_name} #{self.last_name}"
+    # end"
+end
